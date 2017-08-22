001cfbf0e0d79286b0eb507e4d48a36778c2785b
Immo Landwerth <immol@microsoft.com>
2017-08-22 09:23:43 -0700

    Add project files.

---
+++ ProblematicApis/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+
+namespace ConsoleApp11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Console.WindowWidth);
+
+            IList list = new ArrayList();
+            list.Add(1);
+            list.Add("two");
+        }
+    }
+}
